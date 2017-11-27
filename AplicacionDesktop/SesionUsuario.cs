c6cd150f727be6b0acf8101f7048da63ed7bb5ec
Matias Lanneponders Yonadi <mlyonadi@gmail.com>
2017-11-27 20:33:17 -0300

    Revert "Merge branch 'master' of https://github.com/JuanCrespo18/PagoFacilFrba"
    This reverts commit a47f63122549edbb89499fb2171a2fcd1a1132a0, reversing
    changes made to 28a32da0571cd48e7f9ad6de53f8ff5bedc6ab1e.

--- AplicacionDesktop/SesionUsuario.cs
+++ AplicacionDesktop/SesionUsuario.cs
@@ -10,9 +10,8 @@ namespace PagoAgilFrba
     {
         public static SesionUsuario usuario;
         public int id { get; set; }
-        public int rol { get; set; }
+        public String rol { get; set; }
         public String username { get; set; }
-        public int sucursal { get; set; }
         
         public static SesionUsuario user
         {get{
@@ -22,22 +21,5 @@ namespace PagoAgilFrba
         }
 
         private SesionUsuario() { }
-
-        public static void CargarSucursalYRol()
-        {
-            var con = new Conexion();
-            con.query = string.Format("SELECT ROL_ID, SUCX_ID FROM ONEFORALL.USUARIO_X_ROL UR " +
-                "INNER JOIN ONEFORALL.USUARIO_X_SUCURSAL US ON UR.USERX_ID = US.USERX_ID " +
-                "INNER JOIN ONEFORALL.ROLES R ON UR.ROLX_ID = R.ROL_ID " +
-                "WHERE UR.USERX_ID = {0} " +
-                "AND ROL_ACTIVO = 1", user.id);
-            con.leer();
-            if(con.leerReader())
-            {
-                user.rol = con.lector.GetInt32(0);
-                user.sucursal = con.lector.GetInt32(1);
-            }
-            con.cerrarConexion();
-        }
     }
 }
