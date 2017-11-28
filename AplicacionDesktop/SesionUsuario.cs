44c8a488628f9a42a098fb9314cc213cfac20869
Juan Crespo <juanmacrespo87@gmail.com>
2017-11-28 01:21:54 -0300

    Elegir rol y sucursal

--- AplicacionDesktop/SesionUsuario.cs
+++ AplicacionDesktop/SesionUsuario.cs
@@ -10,8 +10,10 @@ namespace PagoAgilFrba
     {
         public static SesionUsuario usuario;
         public int id { get; set; }
-        public List<String> funcionalidades { get; set; }
+        public int rol { get; set; }
         public String username { get; set; }
+        public int sucursal { get; set; }
+        public List<string> funcionalidades { get; set; }
         
         public static SesionUsuario user
         {get{
@@ -22,5 +24,22 @@ namespace PagoAgilFrba
         }
 
         private SesionUsuario() { }
+
+        public static void CargarSucursalYRol()
+        {
+            var con = new Conexion();
+            con.query = string.Format("SELECT ROL_ID, SUCX_ID FROM ONEFORALL.USUARIO_X_ROL UR " +
+                "INNER JOIN ONEFORALL.USUARIO_X_SUCURSAL US ON UR.USERX_ID = US.USERX_ID " +
+                "INNER JOIN ONEFORALL.ROLES R ON UR.ROLX_ID = R.ROL_ID " +
+                "WHERE UR.USERX_ID = {0} " +
+                "AND ROL_ACTIVO = 1", user.id);
+            con.leer();
+            if(con.leerReader())
+            {
+                user.rol = con.lector.GetInt32(0);
+                user.sucursal = con.lector.GetInt32(1);
+            }
+            con.cerrarConexion();
+        }
     }
 }
