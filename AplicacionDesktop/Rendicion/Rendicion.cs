020c673902aae39e42c559fd686c3c82d6dd1558
Juan Crespo <juanmacrespo87@gmail.com>
2017-11-29 22:10:20 -0300

    Modificacion en Rendicion

--- AplicacionDesktop/Rendicion/Rendicion.cs
+++ AplicacionDesktop/Rendicion/Rendicion.cs
@@ -59,7 +59,7 @@ namespace PagoAgilFrba.Rendicion
                     "INNER JOIN ONEFORALL.PAGOS P ON F.FACT_PAGO_ID = P.PAGO_ID " +
                     "INNER JOIN ONEFORALL.EMPRESAS E ON E.EMP_ID = {2} " +
                     "WHERE FACT_REND_ID IS NULL AND " +
-                    "PAGO_FECHA_PAGO > CONVERT(DATETIME,'{1}',101)", cboEmpresas.SelectedItem.ToString(), string.Format("{0}-{1}-{2}", DateTime.Now.Year, DateTime.Now.Month - 1, dia_rendicion), _idEmpresa);
+                    "PAGO_FECHA_PAGO < '{1}'", cboEmpresas.SelectedItem.ToString(), DateTime.Now.ToString("yyyy-mm-dd"), _idEmpresa);
                 con.leer();
                 if (!con.leerReader())
                 {
